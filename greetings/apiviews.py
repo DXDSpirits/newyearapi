fc99dc43ef3257b709d8909448dddc7e722f50fb
Xindong Ding <dxd.spirits@gmail.com>
2016-02-07 13:35:28 +0800

    hotfix

--- greetings/apiviews.py
+++ greetings/apiviews.py
@@ -149,7 +149,7 @@ class GreetingViewSet(DetailSerializerMixin,
 
         auth = Auth(access_key, secret_key)
         pfop = PersistentFop(auth, 'tatmusic', 'wechataudio', notify_url)
-        op = op_save('avthumb/mp3/aq/0', 'tatmusic', instance.key + '.mp3')
+        op = op_save('avthumb/mp3', 'tatmusic', instance.key + '.mp3')
         ret, info = pfop.execute(instance.key, [op])
         if instance.data is None:
             instance.data = ret
